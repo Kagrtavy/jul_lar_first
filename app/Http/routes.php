df729384dafc4d565426c1e1340925f90fa20f95
Kagrtavy <nesterivan97@gmail.com>
2024-12-27 15:32:40 +0200

    added routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,22 +11,67 @@
 |
 */
 
+use \Illuminate\Http\Request;
+use \App\Models\Task;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/task', function (){
-    echo 'all';
+    $tasks = Task::orderBy('created_at', 'desc')->get();
+    return view('task.index', [
+        'tasks'=>$tasks,
+    ]);
 })->name('task.index');
 
 Route::get('/task/create', function (){
-    echo 'add form';
-});
+    return view('task.create');
+})->name('task.create');
 
-Route::post('/task', function (){
+Route::post('/task', function (Request $request){
+    $validator = Validator::make($request->all(),[
+        'name'=>'required|max:10|unique:tasks,name',
+    ]);
+
+    if($validator->fails()){
+        return redirect(route('task.create'))
+            ->withInput()
+            ->withErrors($validator);
+    }
+    $task = new Task();
+    $task->name = $request->name;
+    $task->save();
     echo 'store';
-});
 
-Route::delete('/task/{id}', function (){
-    echo 'delete';
-});
+    return redirect(route('task.index'));
+})->name('task.store');
+
+Route::delete('/task/{id}', function ($id){
+    $task = Task::findOrFail($id);
+    $task->delete();
+    return redirect(route('task.index'));
+})->name('task.destroy');
+
+Route::get('/task/{id}/edit', function ($id) {
+    $task = Task::findOrFail($id);
+    return view('task.edit', ['task' => $task]);
+})->name('task.edit');
+
+Route::put('/task/{id}', function (Request $request, $id) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:10|unique:tasks,name,' . $id,
+    ]);
+
+    if ($validator->fails()) {
+        return redirect(route('task.edit', $id))
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = Task::findOrFail($id);
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect(route('task.index'));
+})->name('task.update');
