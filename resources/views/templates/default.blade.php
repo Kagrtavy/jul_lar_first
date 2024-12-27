f557b55073d376e3c9637627ee654ff52a493a95
Kagrtavy <nesterivan97@gmail.com>
2024-12-27 15:32:55 +0200

    init

---
+++ resources/views/templates/default.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Laravel Quickstart - Basic</title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <!-- CSS And JavaScript -->
+    </head>
+    <body>
+    <div class="container">
+        <nav class="navbar navbar-default">
+            <!-- Navbar Contents -->
+        </nav>
+    </div>
+    @yield('content')
+    </body>
+</html>
